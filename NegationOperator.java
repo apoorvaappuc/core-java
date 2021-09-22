32df2b65c96bb0841c188c597d71324f5af50790
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-22 23:28:49 +0530

    writes negation operator program

---
+++ NegationOperator.java
@@ -0,0 +1,11 @@
+class NegationOperator{
+public static void main(String[] args){
+
+     System.out.println("NEGATION OPERATION......");
+     boolean b=true;
+     boolean Result=!b;
+     System.out.println("b: "+b);
+     System.out.println("Result: "+Result);
+
+}
+}
