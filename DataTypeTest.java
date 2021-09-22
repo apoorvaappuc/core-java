63394fbc04e32903cd46cc4a321d8aa9fc090f17
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-22 17:38:17 +0530

    writes datatypetest

---
+++ DataTypeTest.java
@@ -0,0 +1,29 @@
+class DataTypeTest{
+      public static void main(String[] args){
+           byte b=10;
+           System.out.println("b:" +b);
+
+           short s=30000;
+           System.out.println("s:" +s);
+        
+           int i=1000000;
+           System.out.println("i:" +i);
+
+           long l=100000000000l;
+           System.out.println("l:" +l);
+
+           float f=10.45f;
+           System.out.println("f:" +f);
+
+           double d=865.64;
+           System.out.println("d:" +d);
+
+           char c='8';
+           System.out.println("c:" +c);
+
+           boolean bo=false;
+           System.out.println("bo:" +bo);
+
+
+      }
+}
