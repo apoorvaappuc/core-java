75d12bd44119b8c7a7377f3bf5a81cecce45baec
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-22 23:04:31 +0530

    writes increment operator program

---
+++ IncrementOperator.java
@@ -0,0 +1,16 @@
+class IncrementOperator{
+public static void main(String[] args){
+
+     System.out.println("PRE INCREMENT OPERATION......");
+     int a=1;
+     int b=++a;
+     System.out.println("a: "+a);
+     System.out.println("b: "+b);
+
+     System.out.println("POST INCREMENT OPERATION......");
+     int u=5;
+     int v=u++;
+     System.out.println("u: "+u);
+     System.out.println("v: "+v);
+}
+}
