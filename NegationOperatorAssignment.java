2cbccdff9c2cf229a3f061f24d00c831ffe8cfad
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-23 09:09:33 +0530

    writes negation assignment program

---
+++ NegationOperatorAssignment.java
@@ -0,0 +1,11 @@
+class NegationOperatorAssignment{
+public static void main(String[] args){
+
+     System.out.println("THIS IS NEGATION OPERATION......");
+     boolean b=false;
+     boolean Result=!b;
+     System.out.println("b: "+b);
+     System.out.println("Result: "+Result);
+
+}
+}
