4115de4480ebe9d7bfa52867354c198338f77e23
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-23 09:08:35 +0530

    writes increment assignment program

---
+++ IncrementOperatorAssignment.java
@@ -0,0 +1,16 @@
+class IncrementOperatorAssignment{
+public static void main(String[] args){
+
+     System.out.println("THIS IS PRE INCREMENT OPERATION......");
+     int a=1;
+     int b=++a;
+     System.out.println("a: "+a);
+     System.out.println("b: "+b);
+
+     System.out.println("THIS IS POST INCREMENT OPERATION......");
+     int u=5;
+     int v=u++;
+     System.out.println("u: "+u);
+     System.out.println("v: "+v);
+}
+}
