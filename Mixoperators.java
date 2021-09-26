551f47bc5f8786ac908ff6f2c4d6e3cfb3fce8f1
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-26 11:20:23 +0530

    writes operators program

---
+++ Mixoperators.java
@@ -0,0 +1,22 @@
+class MixOperators{
+public static void main(String[]args){
+
+         int x=10;
+         int y=++x;
+         int z=y++;
+         System.out.println(++z);
+
+         int i=100;
+         i++;
+         int j=++i;
+         int k=j--;
+         System.out.println(--k);
+ 
+         int m=5;
+         m--;
+         --m;
+         int n=m;
+         System.out.println(++n);
+
+}
+}
