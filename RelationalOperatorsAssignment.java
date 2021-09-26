f93f34e4819ea790ef1e4344b1d325bdd577af49
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-26 11:16:42 +0530

    writes RelationalOperators

---
+++ RelationalOperatorsAssignment.java
@@ -0,0 +1,19 @@
+class RelationalOperatorsAssignment{
+public static void main(String[]args){
+
+     int k=7;
+     int l=9;
+     int v=10;
+
+    System.out.println("Result of k>l  is ="+(k>l));
+    System.out.println("Result of k>=l is ="+(k>=l));
+    System.out.println("Result of l<v  is ="+(l<v));
+    System.out.println("Result of l<=v is ="+(l<=v));
+    System.out.println("Result of k==l is ="+(k==l));
+    System.out.println("Result of k!=l is ="+(k!=l));
+    System.out.println("Result of l>v  is ="+(l>v));
+    System.out.println("Result of l!=v is ="+(l!=v));
+    System.out.println("Result of k==v is ="+(k==v));
+
+}
+}
