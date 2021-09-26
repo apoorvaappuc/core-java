2d14df3667058a36f32c37b9a71f407b77197667
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-26 19:37:42 +0530

    wites BitwiseXOR program

---
+++ BitwiseXor.java
@@ -0,0 +1,11 @@
+class BitwiseXor{
+public static void main(String[] args){
+
+  int x=5;
+  int y=7;
+  int z=3;
+  System.out.println("x^y="+(x^y));
+  System.out.println("y^z="+(y^z));
+  System.out.println("x^z="+(x^z));
+}
+}
