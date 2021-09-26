20295ea7fdcd1511f444f4ae2f40ad22a0fbf5e8
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-26 11:10:11 +0530

    writes AssignmentOperatorAssignment

---
+++ AssignmentOperatorAssignment.java
@@ -0,0 +1,25 @@
+class AssignmentOperatorAssignment{
+public static void main(String[] args){
+ 
+       int t=89;
+       int v=10;
+       t*=v;
+       System.out.println("t:"+t);
+
+       int e=80;
+       int f=40;
+       e-=f;
+       System.out.println("e:"+f);
+      
+       short o=40;
+       short p=40;
+       o+=p;
+       System.out.println("o:"+o);
+
+       int h=8;
+       int g=4;
+       h%=g;
+       System.out.println("h:"+h);
+
+}
+}
