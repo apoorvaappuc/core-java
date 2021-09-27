7c5d8fc3864eba926298ca24b47682d72a0a3c26
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-09-27 22:26:29 +0530

    This is Right Shift Operator program

---
+++ RightShiftOperator.java
@@ -0,0 +1,12 @@
+class RightShiftOperator{
+public static void main(String[] args){
+      
+       
+       int a=8;
+       int b=a>>2;
+       System.out.println("THIS IS RIGHT SHIFT OPERATOR.....");
+       System.out.println(b); 
+
+       
+}
+}
