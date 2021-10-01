6db0b0d9857cef9187e9e84cc0fa87c998490ac4
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-01 16:09:51 +0530

    This is even or odd program

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+class EvenOdd{
+public static void main(String[] args){
+       int num=4;
+       
+       
+       if(num%2==0)
+       {
+        System.out.println("THIS IS EVEN NUMBER");
+       }
+       else
+       {
+        System.out.println("THIS IS ODD NUMBER");
+       }
+}
+}
+       
