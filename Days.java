1ff2412a5e7bff0b3f79f1ceef3fef7bd7acd2e0
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-01 16:07:40 +0530

    This is days program

---
+++ Days.java
@@ -0,0 +1,38 @@
+class Days{
+public static void main(String[] args){
+
+       int number=6;
+       if(number==1)
+       {
+         System.out.println("MONDAY");
+       }
+       else if(number==2)
+       {
+         System.out.println("TUESDAY");
+       }
+       else if(number==3)
+       {
+         System.out.println("WEDNESDAY");
+       }
+       else if(number==4)
+       {
+         System.out.println("THURSDAY");
+       }
+       else if(number==5)
+       {
+         System.out.println("FRIDAY");
+       }
+       else if(number==6)
+       {
+         System.out.println("SATURDAY");
+       }
+       else if(number==7)
+       {
+         System.out.println("SUNDAY");
+       }
+       else 
+       {
+         System.out.println("PLEASE ENTER THE NUMBER BETWEEN 1 TO 7");
+       }
+}
+}
