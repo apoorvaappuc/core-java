53bd63b40d99c37f69ca9c79b671ca7683f93723
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-01 16:08:38 +0530

    This is digit program

---
+++ Digit2.java
@@ -0,0 +1,18 @@
+class Digit2{
+public static void main(String[] asrgs){
+
+
+    int num=9964;
+    if(num<10000 && num>=1000)
+    {
+      System.out.println("IT IS 4 DIGIT NUMBER");
+    }
+
+    else
+    {
+      System.out.println("IT IS NOT 4 DIGIT NUMBER");  
+    }
+}
+}
+     
+   
