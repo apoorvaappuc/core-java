d292f4458cffd185da47f1079eb5f1257e691945
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-01 16:08:54 +0530

    This is digit program

---
+++ Digit3.java
@@ -0,0 +1,17 @@
+class Digit3{
+public static void main(String[] asrgs){
+
+
+    int num=9964;
+
+     if(num<1000000 && num>=100000)
+    {
+      System.out.println("IT IS 6 DIGIT NUMBER");
+    }
+     else
+    {
+      System.out.println("IT IS NOT 6 DIGIT NUMBER");
+    }
+     
+}
+}
