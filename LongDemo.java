25a8d1612e1d2498a1101080390583ace967eb4e
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-01 16:11:08 +0530

    This is month program

---
+++ LongDemo.java
@@ -0,0 +1,15 @@
+class LongDemo{
+public static void main(String[] args){
+          
+      long num =-56l;
+       
+       if(num>10)
+       { 
+         System.out.println("VALID NUMBER");
+       }
+       else if(num<10)
+       { 
+         System.out.println("INVALID NUMBER");
+       }
+}
+}
