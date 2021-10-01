6dd4bec914114ec789ed90fe73d6bbfdea80f758
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-01 16:08:19 +0530

    This is digit program

---
+++ Digit1.java
@@ -0,0 +1,16 @@
+class Digit1{
+public static void main(String[] asrgs){
+
+
+    int num=2;
+    if(num <100 && num>=10)
+    {
+       System.out.println("IT IS 2 DIGIT NUMBER");  
+    }
+    else
+    {  
+        System.out.println("IT IS NOT 2 DIGIT NUMBER");  
+    }
+      
+}
+}
