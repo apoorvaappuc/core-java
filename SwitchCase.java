67b725598032c7b945fa957e0dd9cbaee8f86f6f
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-03 21:59:00 +0530

    switch case program

---
+++ SwitchCase.java
@@ -0,0 +1,26 @@
+class SwitchCase{
+public static void main(String[] args){
+      int number=5;
+      switch(number)
+      {
+       case 1:System.out.println("SUN");
+       break;
+       case 2:System.out.println("SAT");
+       break;
+       case 3:System.out.println("FRI");
+       break; 
+       case 4:System.out.println("THR");
+       break;
+       case 5:System.out.println("WED");
+       break;
+       case 6:System.out.println("TUE");
+       break;
+       case 7:System.out.println("MON");
+       break;
+       default:System.out.println("ENTER VALID NUMBER");
+       break;
+}
+}
+}
+       
+
