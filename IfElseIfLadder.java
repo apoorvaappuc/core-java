660d9e0f1c73256a2334d1e5f83b8dc6db718173
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-11 16:21:20 +0530

    IfElseIf Ladder program

---
+++ IfElseIfLadder.java
@@ -0,0 +1,20 @@
+class IfElseIfLadder{
+public static void main(String[]args){
+         boolean aadhar=true;
+         boolean pancard=false;
+         boolean voterId=false;
+         
+         if(voterId){
+         System.out.println("You are INDIAN CITIZEN");
+         }
+          else if(pancard){
+          System.out.println("You are INDIAN CITIZEN");
+         }
+          else if(aadhar){
+          System.out.println("You are INDIAN CITIZEN");
+         }
+         else{
+         System.out.println("You are INDIAN CITIZEN");
+         }
+}
+}
