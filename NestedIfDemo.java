11cb9e9112ab082436121edc01c36b3936c1eb6e
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-11 16:22:40 +0530

    Nested If program

---
+++ NestedIfDemo.java
@@ -0,0 +1,27 @@
+class NestedIfDemo{
+public static void main(String[]args){
+         boolean isAptitudeClear=true;
+         boolean isTechnicalClear=false;
+         boolean isHrRoundClear=false;
+
+         if(isAptitudeClear)
+         {
+            if(isTechnicalClear)
+            {
+              if(isHrRoundClear)
+              {
+                 System.out.println("CONGRADULATIONS......YOU ARE SELECTED");
+              }
+           else
+            
+                 System.out.println("SORRY......YOU ARE NOT SELECTED");
+            }
+        else
+           
+                 System.out.println("SORRY......YOU ARE NOT SELECTED");
+           }
+}
+}
+
+
+         
