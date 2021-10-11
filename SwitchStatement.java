68f413d44bc54c9e2bb69138001953480fcccc29
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-11 16:23:28 +0530

    Switch case program

---
+++ SwitchStatement.java
@@ -0,0 +1,20 @@
+class SwitchStatement{
+public static void main(String[] args){
+
+         char symbol='+';
+         
+         switch(symbol){
+         case '+':System.out.println("ADDITION");
+         break;
+         case '-':System.out.println("SUBTRACTION");
+         break;
+         case '*':System.out.println("MULTIPLICATION");
+         break;
+         case '/':System.out.println("DIVION");
+         break;
+         case '%':System.out.println("MODULES");
+         break;
+         default:System.out.println("INVALID SYMBOL");
+}
+}
+}
