a678a29832e45c64a378108251cbfa3fba4d5b96
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-14 07:41:38 +0530

    This is return program

---
+++ ReturnDemo1.java
@@ -0,0 +1,14 @@
+class ReturnDemo1{
+
+
+         static void findCube(int number){
+         int cube=number*number*number;
+         System.out.println("cube:"+cube);
+          }
+public static void main(String[] args){
+    findCube(5);
+    
+
+
+}
+}
