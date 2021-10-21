4778f5e70d5bd6a428c26ea5c76c8ae95afe4ca6
apoorvaappuc <60443277+apoorvaappuc@users.noreply.github.com>
2021-10-21 20:41:48 +0530

    this is hierarchical inheritance program

---
+++ XworksZ/src/hierarchicalinheritance/HierarchicalInheritence.java
@@ -0,0 +1,21 @@
+package hierarchicalinheritance;
+
+public class HierarchicalInheritence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Audi audi=new Audi();
+      Bmw bmw=new Bmw();
+      Ferrari ferrari=new Ferrari();
+      
+      audi.drive();
+      audi.speed();
+      
+      bmw.drive();
+      bmw.speed();
+      
+      ferrari.drive();
+      ferrari.speed();
+	}
+
+}
